888c7681acb186ab3f1b312db3e4b36d5fdf118b
tevintsales <salest@hawaii.edu>
2017-09-11 15:52:07 -1000

    update to add enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	LevelManager levelManager;
+
+	public float movementSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		levelManager = FindObjectOfType<LevelManager> ();
+	}
+
+	void Update(){
+		transform.Translate (Vector2.left * movementSpeed * Time.deltaTime); 
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			Debug.Log ("Player");
+			levelManager.LoadLevelWithSound("Death Screen");
+		}
+	}
+}
---
+++ Assets/Shredder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy (other);
+	}
+}
